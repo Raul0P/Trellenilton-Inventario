3c63ae6a6421146d94343574aacbdd27341adb1d
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-25 11:35:42 -0300

    add: implement AuthContext with product fetching functionality

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,21 @@
+import { API_PROVIDER } from '@/axios';
+import { IProduto } from '@/interface/axios/response/IProduto';
+import { IAuthContext, IAuthProviderProps } from '@/interface/context/Auth';
+import { createContext, useState } from 'react';
+
+export const AuthContext = createContext<IAuthContext>({} as IAuthContext);
+
+export const AuthProvider = ({ children }: IAuthProviderProps) => {
+  const [produtos, setProdutos] = useState<IProduto[]>([]);
+
+  async function getProdutos() {
+    const res = await API_PROVIDER.getProdutos();
+    setProdutos(res);
+  }
+
+  return (
+    <AuthContext.Provider value={{ produtos, setProdutos, getProdutos }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
