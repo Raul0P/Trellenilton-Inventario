61b19892a05ce0e2b308ca0db5d6c355dc67e2f2
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-25 11:50:49 -0300

    add: extend IAuthContext with product management methods

--- src/interface/context/Auth.ts
+++ src/interface/context/Auth.ts
@@ -8,4 +8,7 @@ export interface IAuthContext {
   produtos: IProduto[];
   setProdutos: (produtos: IProduto[]) => void;
   getProdutos: () => Promise<void>;
+  updateProduct: (produto: IProduto) => Promise<void>;
+  createProduct: (produto: IProduto) => Promise<IProduto>;
+  deleteProduct: (produto: IProduto) => Promise<void>;
 }
