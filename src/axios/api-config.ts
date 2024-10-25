c67d0233ece12ca616e05fa7a6be6a14e8192f2c
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-25 11:23:32 -0300

    add: axios API configuration with request/response interceptors

---
+++ src/axios/api-config.ts
@@ -0,0 +1,49 @@
+import axios, { InternalAxiosRequestConfig } from 'axios';
+
+const URL = 'http://localhost:3000/ap1/v1/';
+
+const init = () => {
+  const token = localStorage.getItem('token');
+  const headers = {
+    Authorization: `Bearer ${token}`
+  };
+  return {
+    token,
+    headers
+  };
+};
+
+const api = axios.create({
+  baseURL: URL,
+  headers: {
+    Authorization: `Bearer ${init().token}`
+  },
+  timeout: 15000,
+  responseType: 'json',
+  method: 'GET'
+});
+
+api.interceptors.request.use(
+  (config: InternalAxiosRequestConfig<any>) => {
+    if (!config.url?.includes('login'))
+      config.headers.Authorization = `Bearer ${init().token}`;
+
+    return config;
+  },
+  (error) => {
+    console.log('REQUEST ERROR');
+    console.error(error);
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.log('RESPONSE ERROR');
+    console.error(error);
+    return Promise.reject(error);
+  }
+);
+
+export default api;
