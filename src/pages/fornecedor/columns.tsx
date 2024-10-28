da4449e2209b5567936fbf96c4661c255ec8a3ba
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-10-28 10:42:58 -0300

    add: create columns definition for supplier products table

---
+++ src/pages/fornecedor/columns.tsx
@@ -0,0 +1,102 @@
+import { ColumnDef } from '@tanstack/react-table';
+import { Button } from '@/components/ui/button';
+import { Edit, Trash } from 'lucide-react';
+import { IProduto } from '@/interface/axios/response/IProduto';
+
+export const columns: ColumnDef<IProduto>[] = [
+  {
+    accessorKey: 'image',
+    header: 'Imagem',
+    cell: ({ row }) => (
+      <img
+        src={row.original.image}
+        alt={row.original.name}
+        width={50}
+        height={50}
+      />
+    )
+  },
+  {
+    accessorKey: 'name',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Nome
+          {column.getIsSorted() === 'asc'
+            ? ' ↑'
+            : column.getIsSorted() === 'desc'
+              ? ' ↓'
+              : ''}
+        </Button>
+      );
+    }
+  },
+  {
+    accessorKey: 'price',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Preço
+          {column.getIsSorted() === 'asc'
+            ? ' ↑'
+            : column.getIsSorted() === 'desc'
+              ? ' ↓'
+              : ''}
+        </Button>
+      );
+    }
+  },
+  {
+    accessorKey: 'description',
+    header: 'Descrição'
+  },
+  {
+    accessorKey: 'supplier',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Fornecedor
+          {column.getIsSorted() === 'asc'
+            ? ' ↑'
+            : column.getIsSorted() === 'desc'
+              ? ' ↓'
+              : ''}
+        </Button>
+      );
+    }
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const product = row.original;
+
+      return (
+        <div className="flex items-center space-x-2">
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => console.log('Edit', product)}
+          >
+            <Edit className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => console.log('Delete', product)}
+          >
+            <Trash className="h-4 w-4" />
+          </Button>
+        </div>
+      );
+    }
+  }
+];
