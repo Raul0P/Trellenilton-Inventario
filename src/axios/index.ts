746d428b263e6a7de2c6c0db1c9d62915a1044c4
DiegoAATrevisan <augustodiego02@gmail.com>
2024-10-29 20:11:56 -0300

    Adicionadas funções da API dos usuários.

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,6 +1,7 @@
 import { IProduto } from '@/interface/axios/response/IProduto';
 import api from './api-config';
 import { IFornecedor } from '@/interface/axios/response/IFornecedor';
+import { IUsuario } from '@/interface/axios/response/IUsuario';
 
 export const API_PROVIDER = {
   getProdutos: async () => {
@@ -53,6 +54,35 @@ export const API_PROVIDER = {
   },
   deleteFornecedor: async (fornecedor: IFornecedor) => {
     const res = await api.delete(`fornecedor/${fornecedor.id}`);
+
+    return res.data;
+  },
+  createUsuario: async (usuario: IUsuario) => {
+    const res = await api.post('usuario', usuario);
+    const data: IUsuario = res.data;
+
+    return data;
+  },
+  loginUsuario: async (usuario: IUsuario) => {
+    const res = await api.post('auth/login', usuario);
+    const data: IUsuario = res.data;
+
+    return data;
+  },
+  getUsuarioById: async (id: number) => {
+    const res = await api.get(`usuario/${id}`);
+    const data: IUsuario = res.data.data;
+
+    return data;
+  },
+  updateUsuario: async (usuario: IUsuario) => {
+    const res = await api.patch(`usuario/${usuario.id}`, usuario);
+    const data: IUsuario = res.data;
+
+    return data;
+  },
+  deleteUsuario: async (usuario: IUsuario) => {
+    const res = await api.delete(`usuario/${usuario.id}`);
 
     return res.data;
   }
---
+++ src/interface/axios/response/IUsuario.ts
@@ -0,0 +1,6 @@
+export interface IUsuario {
+  id?: number;
+  name: string;
+  email: string;
+  password: string;
+}
