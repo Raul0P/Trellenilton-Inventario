1365c2dc034da9097bf25c23ddf44a2d278254b3
DiegoAATrevisan <augustodiego02@gmail.com>
2024-10-29 21:19:57 -0300

    reativando funções de login.

--- src/axios/api-config.ts
+++ src/axios/api-config.ts
@@ -2,16 +2,16 @@ import axios, { InternalAxiosRequestConfig } from 'axios';
 
 const URL = 'http://localhost:3000/api/v1/';
 
-// const init = () => {
-//   const token = localStorage.getItem('token');
-//   const headers = {
-//     Authorization: `Bearer ${token}`
-//   };
-//   return {
-//     token,
-//     headers
-//   };
-// };
+const init = () => {
+  const token = localStorage.getItem('token');
+  const headers = {
+    Authorization: `Bearer ${token}`
+  };
+  return {
+    token,
+    headers
+  };
+};
 
 const api = axios.create({
   baseURL: URL,
@@ -24,19 +24,19 @@ const api = axios.create({
   method: 'GET'
 });
 
-// api.interceptors.request.use(
-//   (config: InternalAxiosRequestConfig<any>) => {
-//     if (!config.url?.includes('login'))
-//       config.headers.Authorization = `Bearer ${init().token}`;
+api.interceptors.request.use(
+  (config: InternalAxiosRequestConfig<any>) => {
+    if (!config.url?.includes('login'))
+      config.headers.Authorization = `Bearer ${init().token}`;
 
-//     return config;
-//   },
-//   (error) => {
-//     console.log('REQUEST ERROR');
-//     console.error(error);
-//     return Promise.reject(error);
-//   }
-// );
+    return config;
+  },
+  (error) => {
+    console.log('REQUEST ERROR');
+    console.error(error);
+    return Promise.reject(error);
+  }
+);
 
 api.interceptors.response.use(
   (response) => response,
