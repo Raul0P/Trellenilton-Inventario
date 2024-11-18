9d983676c762df8c601e27e624b756c973e8e25d
DiegoAATrevisan <augustodiego02@gmail.com>
2024-11-18 01:02:45 -0300

    Adicionadas rotas de cliente para o API_PROVIDER.

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -3,6 +3,7 @@ import api from './api-config';
 import { IFornecedor } from '@/interface/axios/response/IFornecedor';
 import { IUsuario } from '@/interface/axios/response/IUsuario';
 import { ILoginResponse } from '@/interface/axios/response/ILoginResponse';
+import { ICliente } from '@/interface/axios/response/ICliente';
 
 export const API_PROVIDER = {
   getProdutos: async () => {
@@ -88,6 +89,35 @@ export const API_PROVIDER = {
   },
   deleteUsuario: async (usuario: IUsuario) => {
     const res = await api.delete(`user/${usuario.id}`);
+
+    return res.data;
+  },
+  getCliente: async () => {
+    const res = await api.get('cliente');
+    const data: ICliente[] = res.data.data;
+
+    return data;
+  },
+  getClienteById: async (id: number) => {
+    const res = await api.get(`cliente/${id}`);
+    const data: ICliente[] = res.data.data;
+
+    return data;
+  },
+  createCliente: async (cliente: ICliente) => {
+    const res = await api.post('cliente', cliente);
+    const data: ICliente[] = res.data;
+
+    return data;
+  },
+  updateCliente: async (cliente: ICliente) => {
+    const res = await api.patch(`cliente/${cliente.id}`, cliente);
+    const data: ICliente = res.data;
+
+    return data;
+  },
+  deleteCliente: async (cliente: ICliente) => {
+    const res = await api.delete(`cliente/${cliente.id}`);
 
     return res.data;
   }
