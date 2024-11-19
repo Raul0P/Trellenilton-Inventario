bd2d05cfe2aced83d6da7d7634e2658cc0a9af8f
Diego A A Trevisan <augustodiego02@gmail.com>
2024-11-19 15:47:24 -0300

    Adicionada interface de pedidos.

--- src/interface/axios/response/ICliente.ts
+++ src/interface/axios/response/ICliente.ts
@@ -1,9 +1,11 @@
+import { IPedido } from './IPedido';
+
 export interface ICliente {
   id?: number;
   name: string;
   endereco: string;
   cpf_cnpj: string;
   contato: string;
-  pedidos?: string;
+  pedidos?: IPedido[];
   archived: boolean;
 }
